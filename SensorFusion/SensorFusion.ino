cd1acc22da1208b5490ad4f0ca45419419b62509
Nitro <nitrodev0@gmail.com>
2022-12-21 23:50:52 +0900

    Add setup note. Replace the MPU library with one that can be downloaded from the Arduino IDE library manager.

---
+++ SensorFusion/SensorFusion.ino
@@ -0,0 +1,15 @@
+#include <MPU9250.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Arduino_JSON.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
