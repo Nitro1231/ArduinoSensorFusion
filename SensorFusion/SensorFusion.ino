6cdff1b5ae529b5c1ff37eb6a484dc37ab026afb
Nitro <nitrodev0@gmail.com>
2022-12-23 12:25:57 +0900

    Add function to get velocity and position.

--- SensorFusion/SensorFusion.ino
+++ SensorFusion/SensorFusion.ino
@@ -24,6 +24,25 @@ struct RPY {
   float y;
 } rpy;
 
+struct LinAcc {
+  float x;
+  float y;
+  float z;
+} linAcc;
+
+struct LinVel {
+  float x = 0;
+  float y = 0;
+  float z = 0;
+} linVel;
+
+struct Pos {
+  float x = 0;
+  float y = 0;
+  float z = 0;
+} pos;
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -64,32 +83,55 @@ void setup() {
   print_calibration();
   mpu.verbose(false);
 
-  OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/quat", quat.x, quat.y, quat.z, quat.w);
-  // OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/euler", euler.x, euler.y, euler.z);
+
+  // OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/quat", quat.x, quat.y, quat.z, quat.w);
+  OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/euler", euler.x, euler.y, euler.z);
   // OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/rpy", rpy.r, rpy.p, rpy.y);
+  // OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/acc", acc.x, acc.y, acc.z);
+  // OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/lin_acc", linAcc.x, linAcc.y, linAcc.z);
+  // OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/lin_vel", linVel.x, linVel.y, linVel.z);
+  OscWiFi.publish(OSC_SERVER_HOST, OSC_SERVER_PORT, "/pos", pos.x, pos.y, pos.z);
 }
 
 
+unsigned long prevTime = millis();
+unsigned long currTime;
+
 void loop() {
-  // delay(500);
   if (mpu.update()) {
-    // static uint32_t prev_ms = millis();
-    // if (millis() > prev_ms + 25) {
-    //   print_roll_pitch_yaw();
-    //   prev_ms = millis();
-    // }
+    currTime = millis();
 
-    quat.x = mpu.getQuaternionX();
-    quat.y = mpu.getQuaternionY();
-    quat.z = mpu.getQuaternionZ();
-    quat.w = mpu.getQuaternionW();
+    // print_roll_pitch_yaw();
+    // print_acc();
+    print_vel_and_pos();
 
-    // euler.x = mpu.getEulerX();
-    // euler.y = mpu.getEulerY();
-    // euler.z = mpu.getEulerZ();
+    // quat.x = mpu.getQuaternionX();
+    // quat.y = mpu.getQuaternionY();
+    // quat.z = mpu.getQuaternionZ();
+    // quat.w = mpu.getQuaternionW();
+
+    euler.x = mpu.getEulerX();
+    euler.y = mpu.getEulerY();
+    euler.z = mpu.getEulerZ();
+  
     // rpy.r = mpu.getRoll();
     // rpy.p = mpu.getPitch();
     // rpy.y = mpu.getYaw();
+
+    linAcc.x = mpu.getLinearAccX();
+    linAcc.y = mpu.getLinearAccY();
+    linAcc.z = mpu.getLinearAccZ();
+
+    unsigned long timeDiff = currTime - prevTime;
+    linVel.x += mpu.getLinearAccX() * timeDiff / 1000;
+    linVel.y += mpu.getLinearAccY() * timeDiff / 1000;
+    linVel.z += mpu.getLinearAccZ() * timeDiff / 1000;
+
+    pos.x += linVel.x * timeDiff / 1000;
+    pos.y += linVel.y * timeDiff / 1000;
+    pos.z += linVel.z * timeDiff / 1000;
+
+    prevTime = currTime;
   }
   OscWiFi.update();
 }
@@ -116,6 +158,40 @@ void print_roll_pitch_yaw() {
   Serial.print(mpu.getLinearAccY(), 2);
   Serial.print(", ");
   Serial.println(mpu.getLinearAccZ(), 2);
+}
+
+
+void print_acc() {
+  Serial.print("AccX, AccY, AccZ: ");
+  Serial.print(mpu.getAccX(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.getAccY(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.getAccZ(), 2);
+  Serial.print("  ");
+  Serial.print("lin_acc = ");
+  Serial.print(mpu.getLinearAccX(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.getLinearAccY(), 2);
+  Serial.print(", ");
+  Serial.println(mpu.getLinearAccZ(), 2);
+}
+
+
+void print_vel_and_pos() {
+  Serial.print("vx, vy, vz: ");
+  Serial.print(linVel.x, 2);
+  Serial.print(", ");
+  Serial.print(linVel.y, 2);
+  Serial.print(", ");
+  Serial.print(linVel.z, 2);
+  Serial.print("  ");
+  Serial.print("pos = ");
+  Serial.print(pos.x, 2);
+  Serial.print(", ");
+  Serial.print(pos.y, 2);
+  Serial.print(", ");
+  Serial.println(pos.z, 2);
 }
 
 
